95c2f230d6e08b31848a14c80eaba19e274d58b6
tamaghnacoder <83964657+tamaghnacoder@users.noreply.github.com>
2022-11-20 10:25:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,38 @@
 img="";
 function preload(){
-img=loadImage('dog_cat.jpg');
+img=loadImage('dog and cat.jpg');
 }
 
 function setup(){
-    canvas=createCanvas(640,420);
+    canvas=createCanvas(1000,800);
     canvas.center();
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
 }
 function draw(){
-    image(img,0,0,640,420);
+    image(img,0,0,1000,800);
     fill("darkblue");
-    text("Dog", 45, 75);
-    noFill();
     strokeWeight(2.5);
     stroke("darkblue");
+    text("Dog", 45, 75);
+    noFill();
     rect(30,60,450,350);
+
+    fill("darkgreen");
+    strokeWeight(1.5);
+    stroke("darkgreen");
+    text("Cat", 320, 120);
+    noFill();
+    rect(300,90,270,320);
+}
+function modelLoaded(){
+    console.log("model loaded");
+    status=true;
+    objectDetector.detect(img,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.log(error,results);
+    }
+    console.log(results);
 }
